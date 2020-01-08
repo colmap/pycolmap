460196744538d74b8a40510c153f260adf7688d9
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2020-01-08 15:00:49 +0100

    Added binding for image-to-world transformation.

--- main.cc
+++ main.cc
@@ -4,9 +4,13 @@ namespace py = pybind11;
 
 #include "absolute_pose.cc"
 #include "essential_matrix.cc"
+#include "fundamental_matrix.cc"
+#include "transformations.cc"
 
 PYBIND11_MODULE(pycolmap, m) {
     m.doc() = "COLMAP plugin";
-    m.def("absolute_pose_estimation", &absolute_pose_estimation, "Doc");
-    m.def("essential_matrix_estimation", &essential_matrix_estimation, "Doc");
+    m.def("absolute_pose_estimation", &absolute_pose_estimation, "Absolute pose estimation with non-linear refinement.");
+    m.def("essential_matrix_estimation", &essential_matrix_estimation, "LORANSAC + 5-point algorithm.");
+    m.def("fundamental_matrix_estimation", &fundamental_matrix_estimation, "LORANSAC + 7-point algorithm.");
+    m.def("image_to_world", &image_to_world, "Image to world transformation.");
 }
---
+++ transformations.cc
@@ -0,0 +1,71 @@
+// Copyright (c) 2018, ETH Zurich and UNC Chapel Hill.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: Johannes L. Schoenberger (jsch at inf.ethz.ch)
+
+#include <iostream>
+#include <fstream>
+
+#include "colmap/base/camera.h"
+
+using namespace colmap;
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+py::dict image_to_world(
+        const std::vector<Eigen::Vector2d> points2D,
+        const py::dict camera_dict
+) {
+    // Create camera.
+    Camera camera;
+    camera.SetModelIdFromName(camera_dict["type"].cast<std::string>());
+    camera.SetWidth(camera_dict["width"].cast<size_t>());
+    camera.SetHeight(camera_dict["height"].cast<size_t>());
+    camera.SetParams(camera_dict["camera_params"].cast<std::vector<double>>());
+
+    // Image to world.
+    std::vector<Eigen::Vector2d> world_points2D;
+    for (size_t idx = 0; idx < points2D.size(); ++idx) {
+        world_points2D.push_back(camera.ImageToWorld(points2D[idx]));
+    }
+    
+    // Mean focal length.
+    const double mean_focal_length = camera.MeanFocalLength();
+
+    // Success output dictionary.
+    py::dict success_dict;
+    success_dict["world_points"] = world_points2D;
+    success_dict["mean_focal_length"] = mean_focal_length;
+    
+    return success_dict;
+}
