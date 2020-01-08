8c0674142a4a58b297c8c69ba818980545b1eea5
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2020-01-08 15:00:32 +0100

    Added fundamental matrix estimation binding.

---
+++ fundamental_matrix.cc
@@ -0,0 +1,92 @@
+// Copyright (c) 2018, ETH Zurich and UNC Chapel Hill.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: Johannes L. Schoenberger (jsch at inf.ethz.ch)
+
+#include <iostream>
+#include <fstream>
+
+#include "colmap/base/camera.h"
+#include "colmap/estimators/fundamental_matrix.h"
+#include "colmap/optim/loransac.h"
+
+using namespace colmap;
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+py::dict fundamental_matrix_estimation(
+        const std::vector<Eigen::Vector2d> points2D1,
+        const std::vector<Eigen::Vector2d> points2D2,
+        const double max_error = 4.00
+) {
+    SetPRNGSeed(0);
+
+    // Check that both vectors have the same size.
+    assert(points2D1.size() == points2D2.size());
+
+    // Failure output dictionary.
+    py::dict failure_dict;
+    failure_dict["success"] = false;
+
+    // Fundamental matrix estimation parameters.
+    RANSACOptions ransac_options;
+    ransac_options.max_error = max_error;
+    ransac_options.min_inlier_ratio = 0.01;
+    ransac_options.min_num_trials = 1000;
+    ransac_options.confidence = 0.9999;
+    
+    LORANSAC<
+        FundamentalMatrixSevenPointEstimator,
+        FundamentalMatrixEightPointEstimator
+    > ransac(ransac_options);
+
+    // Fundamental matrix estimation.
+    const auto report = ransac.Estimate(points2D1, points2D2);
+
+    if (!report.success) {
+        return failure_dict;
+    }
+
+    // Recover data from report.
+    const Eigen::Matrix3d F = report.model;
+    const size_t num_inliers = report.support.num_inliers;
+    const auto inlier_mask = report.inlier_mask;
+
+    // Success output dictionary.
+    py::dict success_dict;
+    success_dict["success"] = true;
+    success_dict["F"] = F;
+    success_dict["num_inliers"] = num_inliers;
+    
+    return success_dict;
+}
