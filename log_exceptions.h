06382c7e7b12385307e36b19f53ea31c6b18d8b6
Phil26AT <phil.lindenberger@gmail.com>
2021-11-12 16:37:01 +0100

    Cleaned up exception macros

--- log_exceptions.h
+++ log_exceptions.h
@@ -5,17 +5,7 @@
 
 #include <pybind11/pybind11.h>
 
-#define THROW_EXCEPTION(exception, msg) \
-throw TemplateException<exception>(__FILE__, __LINE__, msg);
-
-#define THROW_CUSTOM_CHECK_MSG(condition, exception, msg) \
-if (!condition)  \
-    throw TemplateException<exception>(__FILE__, __LINE__, \
-    __MergeTwoConstChar(__GetCheckString(#condition).c_str(), msg).c_str());
-
-#define THROW_CUSTOM_CHECK(condition, exception) \
-if (!condition)  \
-    throw TemplateException<exception>(__FILE__, __LINE__,__GetCheckString(#condition).c_str());
+namespace py = pybind11;
 
 inline const char* __ColmapGetConstFileBaseName(const char* file) {
   const char* base = strrchr(file, '/');
@@ -47,24 +37,27 @@ inline std::string __GetCheckString(const char* cond_str) {
 
 
 inline std::string __MergeTwoConstChar(const char* expr1, const char* expr2) {
-    return (std::string(expr1) + std::string("\n") + expr2);
+    return (std::string(expr1) + std::string(" ") + expr2);
 }
 
 
-inline void __ThrowCheckImpl(const char* file, const int line, const bool result,
-                       const char* expr_str) {
+inline void __ThrowCheckImpl(const char* file, const int line,
+                             const bool result, const char* expr_str) {
     if (!result) {
-        throw TemplateException<py::value_error>(file, line, __GetCheckString(expr_str).c_str());
+        throw TemplateException<py::value_error>(file, line, 
+                                          __GetCheckString(expr_str).c_str());
     }
 }
 
-inline void __ThrowCheckImplMsg(const char* file, const int line, const bool result,
-                       const char* expr_str, std::string msg) {
+inline void __ThrowCheckImplMsg(const char* file, const int line, 
+                                const bool result, const char* expr_str, 
+                                std::string msg) {
     if (!result) {
         std::stringstream ss;
         ss << expr_str << " : " << msg;
         std::string m = ss.str();
-        throw TemplateException<py::value_error>(file, line, __GetCheckString(m.c_str()).c_str());
+        throw TemplateException<py::value_error>(file, line, 
+                                          __GetCheckString(m.c_str()).c_str());
     }
 }
 
@@ -74,19 +67,34 @@ void __ThrowCheckOpImpl(const char* file, const int line, const bool result,
                          const char* val2_str, const char* op_str) {
   if (!result) {
       std::stringstream ss;
-      ss << val1_str << " " << op_str << " " << val2_str <<" ("<<val1<<" vs. "<<val2<<")";
+      ss << val1_str << " " << op_str << " " << val2_str 
+      <<" ("<<val1<<" vs. "<<val2<<")";
       std::string msg = ss.str();
-      throw TemplateException<py::value_error>(file, line, __GetCheckString(msg.c_str()).c_str());
+      throw TemplateException<py::value_error>(file, line, 
+                                        __GetCheckString(msg.c_str()).c_str());
   }
 }
 
 // Option checker macros. In contrast to glog, this function does not abort the
-// program, but simply returns false on failure.
-#define THROW_CHECK(expr) \
+// program, but simply throws an exception on failure.
+#define THROW_EXCEPTION(exception, msg)                                        \
+throw TemplateException<exception>(__FILE__, __LINE__, msg);
+
+#define THROW_CUSTOM_CHECK_MSG(condition, exception, msg)                      \
+if (!condition)                                                                \
+    throw TemplateException<exception>(__FILE__, __LINE__,                     \
+    __MergeTwoConstChar(__GetCheckString(#condition).c_str(), msg).c_str());
+
+#define THROW_CUSTOM_CHECK(condition, exception)                               \
+if (!condition)                                                                \
+    throw TemplateException<exception>(__FILE__, __LINE__,                     \
+                                        __GetCheckString(#condition).c_str());
+
+#define THROW_CHECK(expr)                                                      \
   __ThrowCheckImpl(__FILE__, __LINE__, (expr), #expr);
 
 #define THROW_CHECK_MSG(expr, msg)                                             \
-  __ThrowCheckImplMsg(__FILE__, __LINE__, (expr), #expr, std::to_string(msg))                                 
+  __ThrowCheckImplMsg(__FILE__, __LINE__, (expr), #expr, std::to_string(msg))
 
 #define THROW_CHECK_OP(name, op, val1, val2)                                   \
   __ThrowCheckOpImpl(__FILE__, __LINE__, (val1 op val2), val1, val2,           \
