170f7416187f5cc4cce49d35198709e0fae7e860
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-01-03 10:14:08 +0100

    Bump to v0.0.3

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pycolmap',
-    version='0.0.2',
+    version='0.0.3',
     author='Mihai Dusmanu',
     author_email='mihai.dusmanu@inf.ethz.ch',
     description='COLMAP bindings',
