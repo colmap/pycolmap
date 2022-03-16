cab780a36bf5c4f970da828e4ab59eba4de03b13
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-03-16 14:35:22 +0100

    Estimate the covariances for (generalized) abs pose

--- estimators/absolute_pose.cc
+++ estimators/absolute_pose.cc
@@ -24,7 +24,8 @@ py::dict absolute_pose_estimation(
         const std::vector<Eigen::Vector3d> points3D,
         Camera& camera,
         const AbsolutePoseEstimationOptions estimation_options,
-        const AbsolutePoseRefinementOptions refinement_options
+        const AbsolutePoseRefinementOptions refinement_options,
+        bool return_covariance
 ) {
     SetPRNGSeed(0);
 
@@ -47,7 +48,9 @@ py::dict absolute_pose_estimation(
     }
 
     // Absolute pose refinement.
-    if (!RefineAbsolutePose(refinement_options, inlier_mask, points2D, points3D, &qvec, &tvec, &camera)) {
+    Eigen::Matrix<double, 6, 6> covariance;
+    if (!RefineAbsolutePose(refinement_options, inlier_mask, points2D, points3D, &qvec, &tvec, &camera,
+                            return_covariance ? &covariance : nullptr)) {
         return failure_dict;
     }
 
@@ -69,6 +72,8 @@ py::dict absolute_pose_estimation(
     success_dict["tvec"] = tvec;
     success_dict["num_inliers"] = num_inliers;
     success_dict["inliers"] = inliers;
+    if (return_covariance)
+        success_dict["covariance"] = covariance;
 
     return success_dict;
 }
@@ -97,10 +102,12 @@ py::dict absolute_pose_estimation(
     abs_pose_refinement_options.refine_focal_length = false;
     abs_pose_refinement_options.refine_extra_params = false;
     abs_pose_refinement_options.print_summary = false;
+    const bool kReturnCovariance = false;
 
     return absolute_pose_estimation(
             points2D, points3D, camera,
-            abs_pose_options, abs_pose_refinement_options);
+            abs_pose_options, abs_pose_refinement_options,
+            kReturnCovariance);
 }
 
 
@@ -201,11 +208,13 @@ void bind_absolute_pose_estimation(py::module& m, py::class_<RANSACOptions> PyRA
                                  const std::vector<Eigen::Vector3d>,
                                  Camera&,
                                  const AbsolutePoseEstimationOptions,
-                                 const AbsolutePoseRefinementOptions
+                                 const AbsolutePoseRefinementOptions,
+                                 bool
                                  )>(&absolute_pose_estimation),
         py::arg("points2D"), py::arg("points3D"), py::arg("camera"),
         py::arg("estimation_options") = est_options,
         py::arg("refinement_options") = ref_options,
+        py::arg("return_covariance") = false,
         "Absolute pose estimation with non-linear refinement.");
 
     m.def(
