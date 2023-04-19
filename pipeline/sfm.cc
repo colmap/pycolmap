d2515968dfbabb47d33c5851ed9e1a93d8f1476c
Grzegorz Ruciński <grucin@gmail.com>
2023-04-19 19:14:25 +0200

    Added input model support in incremental mapping pipeline (#84)
    * Added input model support in incremental mapping pipeline
    
    * Move input path to last parameter.
    
    ---------
    
    Co-authored-by: Grzegorz Ruciński <grucin@mobile4.pro>
    Co-authored-by: Mihai Dusmanu <mihai.dusmanu@gmail.com>

--- pipeline/sfm.cc
+++ pipeline/sfm.cc
@@ -51,16 +51,22 @@ std::map<size_t, Reconstruction> incremental_mapping(
     const py::object database_path_,
     const py::object image_path_,
     const py::object output_path_,
-    const IncrementalMapperOptions& options) {
+    const IncrementalMapperOptions& options,
+    const py::object input_path_) {
     std::string database_path = py::str(database_path_).cast<std::string>();
     THROW_CHECK_FILE_EXISTS(database_path);
     std::string image_path = py::str(image_path_).cast<std::string>();
     THROW_CHECK_DIR_EXISTS(image_path);
+    std::string input_path = py::str(input_path_).cast<std::string>();
     std::string output_path = py::str(output_path_).cast<std::string>();
     CreateDirIfNotExists(output_path);
 
     py::gil_scoped_release release;
     ReconstructionManager reconstruction_manager;
+    if (input_path != "") {
+        THROW_CHECK_DIR_EXISTS(input_path);
+        reconstruction_manager.Read(input_path);
+    }
     IncrementalMapperController mapper(
         &options, image_path, database_path, &reconstruction_manager);
 
@@ -103,12 +109,13 @@ std::map<size_t, Reconstruction> incremental_mapping(
     const py::object image_path_,
     const py::object output_path_,
     const int num_threads,
-    const int min_num_matches) {
+    const int min_num_matches,
+    const py::object input_path_) {
     IncrementalMapperOptions options;
     options.num_threads = num_threads;
     options.min_num_matches = min_num_matches;
     return incremental_mapping(
-        database_path_, image_path_, output_path_, options);
+        database_path_, image_path_, output_path_, options, input_path_);
 }
 
 void init_sfm(py::module& m) {
@@ -191,11 +198,13 @@ void init_sfm(py::module& m) {
               const py::object,
               const py::object,
               const py::object,
-              const IncrementalMapperOptions&)>(&incremental_mapping),
+              const IncrementalMapperOptions&,
+              const py::object)>(&incremental_mapping),
           py::arg("database_path"),
           py::arg("image_path"),
           py::arg("output_path"),
           py::arg("options") = mapper_options,
+          py::arg("input_path") = py::str(""),
           "Triangulate 3D points from known poses");
 
     m.def("incremental_mapping",
@@ -203,12 +212,14 @@ void init_sfm(py::module& m) {
                                                            const py::object,
                                                            const py::object,
                                                            const int,
-                                                           const int)>(
+                                                           const int,
+                                                           const py::object)>(
               &incremental_mapping),
           py::arg("database_path"),
           py::arg("image_path"),
           py::arg("output_path"),
           py::arg("num_threads") = mapper_options.num_threads,
           py::arg("min_num_matches") = mapper_options.min_num_matches,
+          py::arg("input_path") = py::str(""),
           "Triangulate 3D points from known poses");
 }
