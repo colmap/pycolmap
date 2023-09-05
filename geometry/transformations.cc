a67a74c02b2ecc3363a1520cab28ccf5dc568b21
Paul-Edouard Sarlin <Skydes@users.noreply.github.com>
2023-09-05 17:46:46 +0200

    Deprecate pycolmap.{image_to_world,world_to_image} (#165)

--- geometry/transformations.cc
+++ geometry/transformations.cc
@@ -17,60 +17,6 @@ namespace py = pybind11;
 #include "log_exceptions.h"
 
 
-py::dict image_to_world(
-        const std::vector<Eigen::Vector2d> points2D,
-        const colmap::Camera& camera
-) {
-    static unsigned int m_warnings = 0;
-    m_warnings++;
-    if (m_warnings == 1) {
-        std::cerr<<"DeprecationWarning: This function will "
-        <<"be removed in a future release. "
-        <<"Use camera.image_to_world instead."<<std::endl;
-    }
-
-    // Image to world.
-    std::vector<Eigen::Vector2d> world_points2D;
-    for (size_t idx = 0; idx < points2D.size(); ++idx) {
-        world_points2D.push_back(camera.ImageToWorld(points2D[idx]));
-    }
-
-    // Mean focal length.
-    const double mean_focal_length = camera.MeanFocalLength();
-
-    // Success output dictionary.
-    py::dict success_dict;
-    success_dict["world_points"] = world_points2D;
-    success_dict["mean_focal_length"] = mean_focal_length;
-
-    return success_dict;
-}
-
-py::dict world_to_image(
-        const std::vector<Eigen::Vector2d> world_points2D,
-        const colmap::Camera& camera
-) {
-    static unsigned int m_warnings = 0;
-    m_warnings++;
-    if (m_warnings == 1) {
-        std::cerr<<"DeprecationWarning: This function will "
-        <<"be removed in a future release. "
-        <<"Use camera.world_to_image instead."<<std::endl;
-    }
-
-    // World to image.
-    std::vector<Eigen::Vector2d> image_points2D;
-    for (size_t idx = 0; idx < world_points2D.size(); ++idx) {
-        image_points2D.push_back(camera.WorldToImage(world_points2D[idx]));
-    }
-
-    // Success output dictionary.
-    py::dict success_dict;
-    success_dict["image_points"] = image_points2D;
-
-    return success_dict;
-}
-
 void init_transforms(py::module& m) {
     m.def("qvec_to_rotmat", &colmap::QuaternionToRotationMatrix,
           py::arg("qvec"),
@@ -125,53 +71,4 @@ void init_transforms(py::module& m) {
             return std::make_pair(qvec12, tvec12);
         },
         py::arg("qvec1"), py::arg("tvec1"), py::arg("qvec2"), py::arg("tvec2"));
-
-    m.def("world_to_image", [](int model_id,
-        Eigen::Ref<Eigen::VectorXd> camera_params,
-        Eigen::Ref<Eigen::Vector2d> uv,
-        Eigen::Ref<Eigen::Vector2d> xy) {
-        int num_params;
-        switch (model_id) {
-            #define CAMERA_MODEL_CASE(CameraModel)                             \
-            case CameraModel::kModelId:                                        \
-                num_params = CameraModel::kNumParams;                          \
-                THROW_CHECK_EQ(num_params,camera_params.size());               \
-                CameraModel::WorldToImage<double>(                             \
-                    camera_params.data(), uv(0), uv(1),                        \
-                    xy.data(), xy.data()+1);                                   \
-                break;
-            CAMERA_MODEL_SWITCH_CASES
-            #undef CAMERA_MODEL_CASE
-        }
-    }, py::arg("model_id"),
-       py::arg("camera_params").noconvert(),
-       py::arg("uv").noconvert(),
-       py::arg("xy").noconvert());
-
-    // Image-to-world and world-to-image.
-    m.def("image_to_world", &image_to_world, "Image to world transformation.");
-    m.def("world_to_image", &world_to_image, "World to image transformation.");
-
-    m.def("image_to_world", [](int model_id,
-        Eigen::Ref<Eigen::VectorXd> camera_params,
-        Eigen::Ref<Eigen::Vector2d> xy,
-        Eigen::Ref<Eigen::Vector2d> uv) {
-        int num_params;
-        switch (model_id) {
-            #define CAMERA_MODEL_CASE(CameraModel)                             \
-            case CameraModel::kModelId:                                        \
-                num_params = CameraModel::kNumParams;                          \
-                THROW_CHECK_EQ(num_params,camera_params.size());               \
-                CameraModel::ImageToWorld<double>(                             \
-                    camera_params.data(), xy(0), xy(1),                        \
-                    uv.data(), uv.data()+1);                                   \
-                break;
-            CAMERA_MODEL_SWITCH_CASES
-            #undef CAMERA_MODEL_CASE
-        }
-    }, py::arg("model_id"),
-       py::arg("camera_params").noconvert(),
-       py::arg("xy").noconvert(),
-       py::arg("uv").noconvert());
-
 }
