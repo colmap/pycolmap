291deeb50659673eb25aa2e8b5aed3bbd943756a
Paul-Edouard Sarlin <15985472+sarlinpe@users.noreply.github.com>
2023-10-09 10:43:30 +0200

    Estimators cleanup (#175)
    * Remove backward-compatible overloads
    
    * Estimators return None on failure
    
    * Cleanup inlier mask conversion
    
    * Store inlier mask as Eigen vector
    
    * Minor fixes in two_view_geometry
    
    * two_view_geometry follows the COLMAP interface
    
    * Code formatting
    
    * Run clang-format on push only to master
    
    * Return copy
    
    * Older versions of Eigen don't support MatriX/VectorX
    
    * Avoid std::optional
    
    * Vector -> Matrix
    
    * Add code formatting script
    
    * Update README.md
    
    * Refine the camera parameters in-place
    
    * Make TwoViewGeometry read-only dataclass

--- estimators/essential_matrix.cc
+++ estimators/essential_matrix.cc
@@ -21,20 +21,17 @@ namespace py = pybind11;
 using namespace pybind11::literals;
 
 #include "log_exceptions.h"
+#include "utils.h"
 
-py::dict essential_matrix_estimation(
+py::object essential_matrix_estimation(
     const std::vector<Eigen::Vector2d> points2D1,
     const std::vector<Eigen::Vector2d> points2D2,
     Camera& camera1,
     Camera& camera2,
     const RANSACOptions options) {
   SetPRNGSeed(0);
-
-  // Check that both vectors have the same size.
   THROW_CHECK_EQ(points2D1.size(), points2D2.size());
-
-  // Failure output dictionary.
-  py::dict failure_dict("success"_a = false);
+  py::object failure = py::none();
   py::gil_scoped_release release;
 
   // Image to world.
@@ -62,13 +59,13 @@ py::dict essential_matrix_estimation(
   const auto report = ransac.Estimate(world_points2D1, world_points2D2);
 
   if (!report.success) {
-    return failure_dict;
+    return failure;
   }
 
   // Recover data from report.
   const Eigen::Matrix3d E = report.model;
   const size_t num_inliers = report.support.num_inliers;
-  const auto inlier_mask = report.inlier_mask;
+  const auto& inlier_mask = report.inlier_mask;
 
   // Pose from essential matrix.
   std::vector<Eigen::Vector2d> inlier_world_points2D1;
@@ -92,81 +89,22 @@ py::dict essential_matrix_estimation(
                           &points3D);
   cam2_from_cam1.rotation = Eigen::Quaterniond(cam2_from_cam1_rot_mat);
 
-  // Convert vector<char> to vector<int>.
-  std::vector<bool> inliers;
-  for (auto it : inlier_mask) {
-    if (it) {
-      inliers.push_back(true);
-    } else {
-      inliers.push_back(false);
-    }
-  }
-
-  // Success output dictionary.
   py::gil_scoped_acquire acquire;
-  py::dict success_dict("success"_a = true,
-                        "E"_a = E,
-                        "cam2_from_cam1"_a = cam2_from_cam1,
-                        "num_inliers"_a = num_inliers,
-                        "inliers"_a = inliers);
-  return success_dict;
-}
-
-py::dict essential_matrix_estimation(
-    const std::vector<Eigen::Vector2d> points2D1,
-    const std::vector<Eigen::Vector2d> points2D2,
-    Camera& camera1,
-    Camera& camera2,
-    const double max_error_px,
-    const double min_inlier_ratio,
-    const int min_num_trials,
-    const int max_num_trials,
-    const double confidence) {
-  RANSACOptions ransac_options;
-  ransac_options.max_error = max_error_px;
-  ransac_options.min_inlier_ratio = min_inlier_ratio;
-  ransac_options.min_num_trials = min_num_trials;
-  ransac_options.max_num_trials = max_num_trials;
-  ransac_options.confidence = confidence;
-  return essential_matrix_estimation(
-      points2D1, points2D2, camera1, camera2, ransac_options);
+  return py::dict("E"_a = E,
+                  "cam2_from_cam1"_a = cam2_from_cam1,
+                  "num_inliers"_a = num_inliers,
+                  "inliers"_a = ToPythonMask(inlier_mask));
 }
 
 void bind_essential_matrix_estimation(py::module& m) {
   auto est_options = m.attr("RANSACOptions")().cast<RANSACOptions>();
 
   m.def("essential_matrix_estimation",
-        static_cast<py::dict (*)(const std::vector<Eigen::Vector2d>,
-                                 const std::vector<Eigen::Vector2d>,
-                                 Camera&,
-                                 Camera&,
-                                 const RANSACOptions)>(
-            &essential_matrix_estimation),
+        &essential_matrix_estimation,
         "points2D1"_a,
         "points2D2"_a,
         "camera1"_a,
         "camera2"_a,
         "estimation_options"_a = est_options,
-        "LORANSAC + 5-point algorithm.");
-
-  m.def("essential_matrix_estimation",
-        static_cast<py::dict (*)(const std::vector<Eigen::Vector2d>,
-                                 const std::vector<Eigen::Vector2d>,
-                                 Camera&,
-                                 Camera&,
-                                 const double,
-                                 const double,
-                                 const int,
-                                 const int,
-                                 const double)>(&essential_matrix_estimation),
-        "points2D1"_a,
-        "points2D2"_a,
-        "camera1"_a,
-        "camera2"_a,
-        "max_error_px"_a = est_options.max_error,
-        "min_inlier_ratio"_a = est_options.min_inlier_ratio,
-        "min_num_trials"_a = est_options.min_num_trials,
-        "max_num_trials"_a = est_options.max_num_trials,
-        "confidence"_a = est_options.confidence,
         "LORANSAC + 5-point algorithm.");
 }
