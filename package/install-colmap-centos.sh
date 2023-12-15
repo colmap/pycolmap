bb1732139c800ddf5336bdfc9e5e24053351d47d
Paul-Edouard Sarlin <15985472+sarlinpe@users.noreply.github.com>
2023-12-15 20:28:04 +0100

    Build wheels with cibuildwheel (#211)

--- package/install-colmap-centos.sh
+++ package/install-colmap-centos.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-PYTHON_VERSIONS=("cp38-cp38" "cp39-cp39" "cp310-cp310" "cp311-cp311")
-
+set -e -x
 uname -a
-echo "Current CentOS Version:"
-cat /etc/centos-release
-
-ls -ltrh /io/
 
 CURRDIR=$(pwd)
-echo "Num. processes to use for building: $(nproc)"
+echo "${CURRDIR}"
 
 # ------ Install dependencies from the default repositories ------
-cd $CURRDIR
+cd ${CURRDIR}
 yum install -y \
     wget \
     git \
@@ -29,9 +24,7 @@ yum install -y \
     lz4-devel
 
 # ------ Install boost ------
-cd $CURRDIR
-#yum install -y centos-release-scl-rh devtoolset-7-gcc-c++
-mkdir -p boost && cd boost
+cd ${CURRDIR}
 export BOOST_FILENAME=boost_1_71_0
 wget -nv https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/${BOOST_FILENAME}.tar.gz
 tar xzf ${BOOST_FILENAME}.tar.gz
@@ -40,66 +33,49 @@ cd ${BOOST_FILENAME}
 ./b2 -j$(nproc) cxxflags="-fPIC" variant=release link=shared --disable-icu install
 
 # ------ Install gflags ------
-cd $CURRDIR
+cd ${CURRDIR}
 git clone --branch v2.2.2 --depth 1 https://github.com/gflags/gflags.git
-cd glflags
+cd gflags
 mkdir build && cd build
-cmake ..
-make -j$(nproc)
-make install
+cmake .. -DBUILD_SHARED_LIBS=ON
+make -j$(nproc) install
 
 # ------ Install glog ------
-cd $CURRDIR
+cd ${CURRDIR}
 git clone --branch v0.6.0 --depth 1 https://github.com/google/glog.git
 cd glog
 mkdir build && cd build
 cmake ..
-make -j$(nproc)
-make install
-
-# Disable CGAL since it pulls many dependencies and increases the wheel size
-#yum install -y yum-utils
-#yum-config-manager --add-repo=http://springdale.princeton.edu/data/springdale/7/x86_64/os/Computational/
-#yum install -y --nogpgcheck CGAL-devel
+make -j$(nproc) install
 
 # ------ Install Eigen ------
-cd $CURRDIR
+cd ${CURRDIR}
 EIGEN_VERSION="3.3.9"
-export EIGEN_DIR="$CURRDIR/eigen-${EIGEN_VERSION}"
+export EIGEN_DIR="${CURRDIR}/eigen"
 wget https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_VERSION}/eigen-${EIGEN_VERSION}.tar.gz
 tar -xvzf eigen-${EIGEN_VERSION}.tar.gz
-cd $EIGEN_DIR
+mv eigen-${EIGEN_VERSION} ${EIGEN_DIR}
+cd ${EIGEN_DIR}
 mkdir build && cd build
 cmake ..
 
 # ------ Install CERES solver ------
-cd $CURRDIR
+cd ${CURRDIR}
 git clone https://ceres-solver.googlesource.com/ceres-solver
 cd ceres-solver
 git checkout $(git describe --tags) # Checkout the latest release
-mkdir build
-cd build
+mkdir build && cd build
 cmake .. -DBUILD_TESTING=OFF \
          -DBUILD_EXAMPLES=OFF \
-         -DEigen3_DIR="$EIGEN_DIR/cmake/"
-make -j$(nproc)
-make install
-
-# ------ Build FreeImage from source and install ------
-#cd $CURRDIR
-#wget http://downloads.sourceforge.net/freeimage/FreeImage3180.zip
-#unzip FreeImage3180.zip
-#cd FreeImage
-#make
-#make install
+         -DEigen3_DIR="${EIGEN_DIR}/cmake/"
+make -j$(nproc) install
 
 # ------ Build COLMAP ------
-cd $CURRDIR
+cd ${CURRDIR}
 git clone https://github.com/colmap/colmap.git
 cd colmap
 git checkout c0355417328f3706a30a9265fd52bc7a5aa4cb8c
-mkdir build/
-cd build/
+mkdir build && cd build
 CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DBoost_USE_STATIC_LIBS=OFF \
          -DBOOST_ROOT=/usr/local \
@@ -107,35 +83,4 @@ CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DCGAL_ENABLED=OFF \
          -DGUI_ENABLED=OFF \
          -DEIGEN3_INCLUDE_DIRS=$EIGEN_DIR
-
-if [ $ec -ne 0 ]; then
-    echo "Error:"
-    cat ./CMakeCache.txt
-    exit $ec
-fi
-set -e -x
 make -j$(nproc) install
-
-# ------ Build pycolmap wheel ------
-cd /io/
-WHEEL_DIR="wheels/"
-for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
-    PYTHON_EXEC="/opt/python/${PYTHON_VERSION}/bin/python"
-    ${PYTHON_EXEC} -m pip install --upgrade pip
-    ${PYTHON_EXEC} -m pip wheel \
-        --no-deps \
-        -w ${WHEEL_DIR} \
-        --config-settings=cmake.define.EIGEN3_INCLUDE_DIRS="${EIGEN_DIR}" \
-        .
-done
-
-PYTHON_DEFAULT="/opt/python/${PYTHON_VERSIONS[-1]}/bin/python"
-${PYTHON_DEFAULT} -m pip install auditwheel
-
-# Bundle external shared libraries into the wheels
-OUT_DIR="/io/wheelhouse"
-mkdir -p ${OUT_DIR}
-for whl in ${WHEEL_DIR}/*.whl; do
-    auditwheel repair "$whl" -w ${OUT_DIR} --plat ${PLAT}
-done
-ls -ltrh ${OUT_DIR}
