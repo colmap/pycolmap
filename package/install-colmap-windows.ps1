8ad5ced352a6f19fee7c4ef726fa3382aa542758
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2023-12-18 15:18:18 +0100

    Fix

--- package/install-colmap-windows.ps1
+++ package/install-colmap-windows.ps1
@@ -1,7 +1,5 @@
 $CURRDIR = $PWD
 
-$env:VCPKG_INSTALLATION_ROOT
-Dir -Recurse ${env:VCPKG_INSTALLATION_ROOT} | Get-Childitem
 & "${env:VCPKG_INSTALLATION_ROOT}/vcpkg.exe" integrate install
 
 curl.exe -L -o "ninja.zip" "https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip"
@@ -14,7 +12,8 @@ git clone https://github.com/colmap/colmap.git
 cd colmap
 git checkout c0355417328f3706a30a9265fd52bc7a5aa4cb8c
 
-& "${env:VCPKG_INSTALLATION_ROOT}/vcpkg.exe" install --recurse ".azure-pipelines/build-windows-vcpkg.txt" --clean-after-build
+$packages = Get-Content -Path ".azure-pipelines/build-windows-vcpkg.txt"
+& "${env:VCPKG_INSTALLATION_ROOT}/vcpkg.exe" install --recurse --clean-after-build @$packages
 
 mkdir build
 cd build
