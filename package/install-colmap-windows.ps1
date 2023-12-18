198486f31019424cfac8da62efa9219164e2a6bb
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2023-12-18 14:58:28 +0100

    Fix

--- package/install-colmap-windows.ps1
+++ package/install-colmap-windows.ps1
@@ -1,10 +1,10 @@
 $CURRDIR = $PWD
 
 $env:VCPKG_INSTALLATION_ROOT
-Dir -Recurse ${VCPKG_INSTALLATION_ROOT} | Get-Childitem
-& "${VCPKG_INSTALLATION_ROOT}\vcpkg.exe" integrate install
+Dir -Recurse ${env:VCPKG_INSTALLATION_ROOT} | Get-Childitem
+& "${env:VCPKG_INSTALLATION_ROOT}/vcpkg.exe" integrate install
 
-curl -L -o "ninja.zip" "https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip"
+curl.exe -L -o "ninja.zip" "https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip"
 Expand-Archive -LiteralPath "${CURRDIR}/ninja.zip" -DestinationPath ${CURRDIR}
 $NINJA_PATH = "${CURRDIR}/ninja.exe"
 
@@ -14,7 +14,7 @@ git clone https://github.com/colmap/colmap.git
 cd colmap
 git checkout c0355417328f3706a30a9265fd52bc7a5aa4cb8c
 
-& "${VCPKG_INSTALLATION_ROOT}/vcpkg.exe" install --recurse ".azure-pipelines/build-windows-vcpkg.txt" --clean-after-build
+& "${env:VCPKG_INSTALLATION_ROOT}/vcpkg.exe" install --recurse ".azure-pipelines/build-windows-vcpkg.txt" --clean-after-build
 
 mkdir build
 cd build
@@ -22,7 +22,7 @@ cmake .. `
   -GNinja `
   -DCMAKE_MAKE_PROGRAM=${NINJA_PATH} `
   -DCMAKE_BUILD_TYPE=Release `
-  -DCMAKE_TOOLCHAIN_FILE="${VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake" `
+  -DCMAKE_TOOLCHAIN_FILE="${env:VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake" `
   -DVCPKG_TARGET_TRIPLET=x64-windows `
   -DCUDA_ENABLED=OFF `
   -DCGAL_ENABLED=OFF `
