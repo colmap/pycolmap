1dccad67d2c83209f732ff98af2d417d8121e02b
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2024-01-15 23:44:17 +0100

    Update example

--- example.py
+++ example.py
@@ -3,6 +3,8 @@ import urllib.request
 import zipfile
 from pathlib import Path
 
+import enlighten
+
 import pycolmap
 from pycolmap import logging
 
@@ -28,13 +30,23 @@ def run():
     if database_path.exists():
         database_path.unlink()
     pycolmap.extract_features(database_path, image_path)
-
     pycolmap.match_exhaustive(database_path)
+    num_images = pycolmap.Database(database_path).num_images
 
     if sfm_path.exists():
         shutil.rmtree(sfm_path)
     sfm_path.mkdir(exist_ok=True)
-    recs = pycolmap.incremental_mapping(database_path, image_path, sfm_path)
+
+    with enlighten.Manager() as manager:
+        with manager.counter(total=num_images, desc="Images registered:") as pbar:
+            pbar.update(0, force=True)
+            recs = pycolmap.incremental_mapping(
+                database_path,
+                image_path,
+                sfm_path,
+                initial_image_pair_callback=lambda: pbar.update(2),
+                next_image_callback=lambda: pbar.update(1),
+            )
     for idx, rec in recs.items():
         logging.info(f"#{idx} {rec.summary()}")
 
